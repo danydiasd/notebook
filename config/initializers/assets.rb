c487ae9cf5dd85e28fdb380f0508f7e77a8cf7f4
Daniele <danyelle.diasd@gmail.com>
2020-10-29 02:34:52 -0300

    Modificcões ate o I18n - realizado o merge

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,14 +3,14 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-<<<<<<< HEAD
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-=======
+
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
@@ -20,4 +20,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
->>>>>>> afaf4a376d5ab7af2fa8c3e127c2efc7aa68265e
